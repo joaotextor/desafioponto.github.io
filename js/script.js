ddf9054d59c5ee90508685452ab58939e956139e
João Textor <joaotextor@gmail.com>
2022-11-15 17:16:33 -0300

    Finished fetching API and dinamically adding items

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //~ CACHE ELEMENTS
+btnLoadMore = document.getElementById('btn-load-more')
+itemList = document.querySelector('.shop-items')
 
+//~ COMMON VARIABLES
+
+let products = 'oiaa'
+let nextPage = ''
 
 //~ FUNCTIONS
 
@@ -16,12 +22,40 @@ const fetchItems = async (url) => {
     .then(toJson)
     .catch(showError)
 
-    console.log(shopItems)
+    products = shopItems.products
+    nextPage = shopItems.nextPage
+
+    console.log(shopItems) //! APAGAR DEPOIS
 }
 
+async function populateItems(url) {
+    await fetchItems(url)
+    btnLoadMore.onclick = () => populateItems(`http://${nextPage}`)
+    products.forEach((product) => {
+        let halfPrice = 0
+        
+        //* replace '.' for ',' and add zero(es) after the number
+        product.price % 2 == 0 
+        ? halfPrice = (product.price / 2).toString()+',00' 
+        : halfPrice = (product.price / 2).toString().replace(".", ",")+'0'
 
+        itemList.innerHTML += `
+        <article class="shop-item flex-column">
+            <img src="${product.image}" alt="Item">
+            <div class="flex-column">
+                <h4 class="fw-normal fs-14">${product.name}</h4>
+                <p class="item-description">${product.description}</p>
+                <p>De: ${product.oldPrice},00</p>
+                <p class="fw-bold fs-16">Por: ${product.price},00</p>
+                <p>ou 2x de ${halfPrice}</p>
+                <button class="body-button fs-16">Comprar</button>
+            </div>
+        </article>
+        `
+    })
+
+}
 
 //~ FUNCTION ATTRIBUTION
 
-
-window.onload = () => fetchItems('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1')
+window.onload = () => populateItems('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1')
