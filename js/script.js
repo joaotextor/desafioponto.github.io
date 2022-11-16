020e19754eaff5c4f6bb36aeaa42b52220d00509
João Textor <joaotextor@gmail.com>
2022-11-16 15:40:49 -0300

    trying to fix a fetch issue on live preview

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ const assignVariables = (shopItems) => {
 }
 
 const assembleHtml = () => {
-    btnLoadMore.onclick = () => populateItems(`https://${nextPage}`)
-    console.log(`https://${nextPage}`)
+    btnLoadMore.onclick = () => populateItems(`http://${nextPage}`)
+    console.log(`http://${nextPage}`)
     products.forEach((product) => {
         let halfPrice = 0
         
